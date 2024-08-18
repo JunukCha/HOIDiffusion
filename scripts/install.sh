1ad4050217f09c3ac4e4bd38f8432431663db1c2
unist <jucha@unist.ac.kr>
2024-08-18 16:53:13 +0900

    pyrender open 3d numpy ligtning

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,15 @@
 conda create -n hoi_diffusion_cha python=3.8 -y
 conda activate hoi_diffusion_cha
 
+pip install pip==23.0
 pip install -r requirements.txt
 pip install git+https://github.com/otaheri/chamfer_distance.git
 pip install git+https://github.com/otaheri/MANO.git
 pip install git+https://github.com/otaheri/bps_torch.git
 pip install git+https://github.com/MPI-IS/mesh.git
 pip install "git+https://github.com/facebookresearch/pytorch3d.git"
+
+pip install numpy==1.23
 
 cd third_party/test
 git clone https://github.com/otaheri/GrabNet
